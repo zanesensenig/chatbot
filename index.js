383c9c6d45439f166164a8634d3ce945339a86b8
Zane Sensenig Admin <zus10a@caa-aadl-a063.borland.psu.edu>
2017-03-02 09:28:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+// Routes
+app.get('/', function(req, req) {
+	res.send("Hi I am a chatbot")
+})
+
+// Facebook
+
+app.get('/webhook/', function(req, res) {
+	if(req.query['hub.verify_token'] === "chatwithzane") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
+})
+
+// Start the server
+app.listen(app.get('port'), function(){
+	console.log("running: port")
+})
